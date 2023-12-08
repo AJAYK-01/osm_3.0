3b4f5f2cc7a44744aef5c3a17694656bde43a8e0
Ajay Krishna K V <55079486+AJAYK-01@users.noreply.github.com>
2023-12-08 23:11:30 +0530

    Add basic location tracking

--- app/lib/pages/map_page.dart
+++ app/lib/pages/map_page.dart
@@ -1,14 +1,19 @@
+import 'package:app/services/location_service.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:syncfusion_flutter_maps/maps.dart';
 
 class MapPage extends StatefulWidget {
   MapPage({super.key});
+
+  final LocationService _locationService = LocationService();
 
   @override
   State<MapPage> createState() => _MapPageState();
 }
 
 class _MapPageState extends State<MapPage> {
+  late Position currLocation;
   late MapTileLayerController _mapController;
   late MapZoomPanBehavior _mapZoomPanController;
 
@@ -21,77 +26,119 @@ class _MapPageState extends State<MapPage> {
       showToolbar: false,
     );
 
+    // just keep track of the location always when available
+    var currLocationStream = LocationService();
+    currLocationStream.livePosition().listen((Position newPosition) {
+      if (currLocation != newPosition) {
+        currLocation = newPosition;
+        _mapController.updateMarkers([0]);
+        // widget._mapZoomPanController.focalLatLng =
+        //     MapLatLng(currLocation.latitude, currLocation.longitude);
+      }
+    });
+
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        GestureDetector(
-          onTapDown: (TapDownDetails details) {
-            final RenderBox box = context.findRenderObject() as RenderBox;
-            final Offset localPosition =
-                box.globalToLocal(details.globalPosition);
-            final MapLatLng currlatLng =
-                _mapController.pixelToLatLng(localPosition);
-
-            showModalBottomSheet(
-              context: context,
-              builder: (BuildContext context) {
-                return Text(
-                  'latitude: ${currlatLng.latitude} longitude: ${currlatLng.longitude}',
-                );
-              },
-            );
-          },
-          child: SfMaps(
-            layers: [
-              MapTileLayer(
-                zoomPanBehavior: _mapZoomPanController,
-                initialMarkersCount: 1,
-                urlTemplate: "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
-                initialZoomLevel: 15,
-                initialFocalLatLng: const MapLatLng(12.203000, 12.220080),
-                controller: _mapController,
-                markerBuilder: (BuildContext context, int index) {
-                  return const MapMarker(
-                      latitude: 12.220080, longitude: 12203000);
-                },
-              ),
-            ],
-          ),
-        ),
-        Positioned(
-          right: 10,
-          bottom: 10,
-          child: Row(
+    currLocation = Position(
+        longitude: 0,
+        latitude: 0,
+        timestamp: DateTime.now(),
+        accuracy: 0,
+        altitude: 0,
+        heading: 0,
+        speed: 0,
+        speedAccuracy: 0,
+        altitudeAccuracy: 0.0,
+        headingAccuracy: 0.0);
+    return FutureBuilder(
+      future: widget._locationService.determinePosition(),
+      builder: (_, initLocationSnapshot) {
+        if (initLocationSnapshot.hasData) {
+          currLocation = initLocationSnapshot.data!;
+          return Stack(
             children: [
-              FloatingActionButton(
-                child: const Icon(Icons.add),
-                onPressed: () {
-                  if (_mapZoomPanController.zoomLevel <
-                      _mapZoomPanController.maxZoomLevel) {
-                    _mapZoomPanController.zoomLevel += 1;
-                  }
+              GestureDetector(
+                onTapDown: (TapDownDetails details) {
+                  final RenderBox box = context.findRenderObject() as RenderBox;
+                  final Offset localPosition =
+                      box.globalToLocal(details.globalPosition);
+                  final MapLatLng currlatLng =
+                      _mapController.pixelToLatLng(localPosition);
+
+                  showModalBottomSheet(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return Text(
+                        'latitude: ${currlatLng.latitude} longitude: ${currlatLng.longitude}',
+                      );
+                    },
+                  );
                 },
+                child: SfMaps(
+                  layers: [
+                    MapTileLayer(
+                      zoomPanBehavior: _mapZoomPanController,
+                      initialMarkersCount: 10,
+                      urlTemplate:
+                          "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+                      initialZoomLevel: 15,
+                      initialFocalLatLng: MapLatLng(
+                          initLocationSnapshot.data!.latitude,
+                          initLocationSnapshot.data!.longitude),
+                      controller: _mapController,
+                      markerBuilder: (BuildContext context, int index) {
+                        return MapMarker(
+                            latitude: initLocationSnapshot.data!.latitude,
+                            longitude: initLocationSnapshot.data!.longitude);
+                      },
+                    ),
+                  ],
+                ),
               ),
-              const SizedBox(
-                width: 8,
-              ),
-              FloatingActionButton(
-                child: const Icon(Icons.remove),
-                onPressed: () {
-                  if (_mapZoomPanController.zoomLevel >
-                      _mapZoomPanController.minZoomLevel) {
-                    _mapZoomPanController.zoomLevel -= 1;
-                  }
-                },
-              ),
+              Positioned(
+                right: 10,
+                bottom: 10,
+                child: Row(
+                  children: [
+                    FloatingActionButton(
+                      child: const Icon(Icons.add),
+                      onPressed: () {
+                        if (_mapZoomPanController.zoomLevel <
+                            _mapZoomPanController.maxZoomLevel) {
+                          _mapZoomPanController.zoomLevel += 1;
+                        }
+                      },
+                    ),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    FloatingActionButton(
+                      child: const Icon(Icons.remove),
+                      onPressed: () {
+                        if (_mapZoomPanController.zoomLevel >
+                            _mapZoomPanController.minZoomLevel) {
+                          _mapZoomPanController.zoomLevel -= 1;
+                        }
+                      },
+                    ),
+                  ],
+                ),
+              )
             ],
-          ),
-        )
-      ],
+          );
+        } else {
+          return const SizedBox(
+            height: 500,
+            width: 500,
+            child: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+      },
     );
   }
 }
---
+++ app/lib/services/location_service.dart
@@ -0,0 +1,41 @@
+import 'package:geolocator/geolocator.dart';
+
+class LocationService {
+  /// Determine the current position of the device.
+  ///
+  /// When the location services are not enabled or permissions
+  /// are denied the `Future` will return an error.
+  Future<Position> determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return await Geolocator.getCurrentPosition();
+  }
+
+  Stream<Position> livePosition() {
+    Stream<Position> currentPosition = Geolocator.getPositionStream();
+    return currentPosition;
+  }
+}
